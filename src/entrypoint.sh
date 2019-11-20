d70d444d8191e348141c9fb73b67c2576f41d156
jeokim75 <54966534+jeokim75@users.noreply.github.com>
2019-11-20 15:35:59 +0900

    Update entrypoint.sh

--- src/entrypoint.sh
+++ src/entrypoint.sh
@@ -6,7 +6,7 @@ source "/opt/ros/dashing/setup.bash"
 
 # setup turtlebot environment
 source "/turtlebot3_ws/install/setup.bash"
-export ROS_DOMAIN_ID=30
+export ROS_DOMAIN_ID=40
 export TURTLEBOT3_MODEL=burger
 
 exec "$@"
